04096eeedb31e8304bb717c09ff63cb43a79ab45
nitinvarda <nitinvardha09@gmail.com>
2023-09-18 23:50:41 -0500

    finalised package versions

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import {View,Text} from 'react-native'
+
+
+
+const App = () => {
+
+
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+
+
+
+}
+
+export default App
